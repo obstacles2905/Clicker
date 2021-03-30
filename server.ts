b975ed61bc20942a62b1c6c4c134ff5349f94c55
Nikita <pismennikita@gmail.com>
2021-03-30 20:31:14 +0300

    minor fixes

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import express, {Express} from "express";
+import express from "express";
 import * as bodyParser from "body-parser";
 import * as dotenv from "dotenv";
 import {router} from "./src/routes/clickerRouter";
@@ -8,22 +8,20 @@ dotenv.config();
 
 const port = process.env.APPLICATION_PORT;
 
-const app: Express = express();
+const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use("/", router);
 
-app.listen(port, () => {
-  console.info(`Server is running on ${port}`);
-});
+const server = app.listen(port, () => {logger.info(`Server is running on ${port}`)});
 
 const sigs = ["SIGINT", "SIGTERM", "SIGQUIT"];
 sigs.forEach((sig) => {
     process.on(sig, () => {
         logger.info(`${sig} called, shutdown application`);
-        app.close(() => {
+        server.close(() => {
             process.exit(0);
         });
     });
